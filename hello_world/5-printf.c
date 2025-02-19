ee721d5269734c6615e42cd3ebcb7eac669d4c8b
farida3326 <farida.esobky@laposte.net>
2025-02-19 08:38:56 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description: Affiche une phrase avec une grammaire correcte.
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
