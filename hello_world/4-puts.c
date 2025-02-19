e41d342089b2075b50f14751a93fd7eac4783dce
farida3326 <farida.esobky@laposte.net>
2025-02-19 08:44:58 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
  * main - Point d'entrée du programme
  *
  * Return: Toujours 0 (Succès)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
